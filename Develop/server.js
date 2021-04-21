e0f04f7a7ffe38dcde2761edf3a4c8b43c05c6de
Greg Harris <ghart.productions@gmail.com>
2021-04-20 17:13:04 -0700

    started building up server.js

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+// Server PORT
+const PORT = process.env.PORT || 3000;
+
+// Initialize Express
+const app = express();
+
+// Required Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve public folder
+app.use(express.static('public'));
+
+// Connect to mongodb at fitness
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// Routes
+app.use(require());
+
+// Listen to PORT 3000
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
