afcac2067d67f19b2dc565455ff20dfe94c9ba08
Greg Harris <ghart.productions@gmail.com>
2021-04-21 15:54:45 -0700

    added to Workout Schema

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -4,9 +4,34 @@ const Schema = mongoose.Schema;
 
 //build schema
 const workoutSchema = new Schema({
-    day: {},
-    exercise: []
-
+    day: {
+        type: Date
+    },
+    exercise: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
 });
 
 const Workout = mongoose.model('Workout', workoutSchema);
