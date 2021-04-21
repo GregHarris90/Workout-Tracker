b9482d2f5d43010c8ee6ffda4b5956e3022ec0e3
Greg Harris <ghart.productions@gmail.com>
2021-04-21 16:09:05 -0700

    updated day part os schema

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 //build schema
 const workoutSchema = new Schema({
     day: {
-        type: Date
+        type: Date,
+        default: Date.now()
     },
     exercise: [
         {
