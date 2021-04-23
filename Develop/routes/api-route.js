b4453100e615290f60f286c183c1b5ef2eb97685
Greg Harris <ghart.productions@gmail.com>
2021-04-23 02:28:24 -0700

    added GET api route for workouts/range

--- Develop/routes/api-route.js
+++ Develop/routes/api-route.js
@@ -1,48 +1,63 @@
 // Dependencies
-const router = require('express').Router();
-const Workout = require('../models/Workout.js');
+// const router = require('express').Router();
+// const Workout = require('../models/Workout.js');
+const db = require('../models');
 
 // API Routes
 // get last workout "/api/workouts" GET
 
-router.get('/api/workouts', (req, res) => {
-    Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+module.exports = function (app) {
+    app.get('/api/workouts', (req, res) => {
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
 
-// add (update) exercise "/api/workouts/" PUT (id)
+    // add (update) exercise "/api/workouts/" PUT (id)
 
-router.put('/api/workouts/:id', (req, res) => {
-    Workout.updateOne({ _id: req.params.id })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+    app.put('/api/workouts/:id', ({ body }, res) => {
+        db.Workout.updateOne({ _id: req.params.id })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
 
-// create workout "/api/workouts" POST
+    // create workout "/api/workouts" POST
 
-router.post('/api/workouts', (req, res) => {
-    Workout.create(body)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+    app.post('/api/workouts', ({ body }, res) => {
+        db.Workout.create(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
 
-// get workouts in range "/api/workouts/range" GET
+    // get workouts in range "/api/workouts/range" GET
 
-// delete workouts DELETE
+    app.get('/api/workouts/range', (req, res) => {
+        db.Workout.find({})
+            .sort({ 'day': -1 })
+            .limit(10)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    })
+    // delete workouts DELETE
 
+
+};
 // export router
 
-module.export = router;
+// module.export = router;
